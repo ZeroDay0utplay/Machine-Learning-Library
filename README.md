8a8f94bd832c1ec192bc1274d7e1962ff659a287
ZeroDay0utplay <hama.tm662@gmail.com>
2023-01-07 19:41:03 +0100

    first version of linear regression

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Machine Learning Package
 ### This is a from-scratch implementation of most of ML learning algorithms
+### The package (.whl file) is located in dist folder
+### All you need to do is: pip install dist/*.whl
 # Supervised ML algorithms
 ## Linear Regression
+### start by "import LinearRegression.LinearRegression as LinearRegression"
--- src/mlib/LinearRegression.py
+++ src/mlib/LinearRegression.py
@@ -1,8 +1,34 @@
+import numpy as np
+
 class LinearRegression:
-    def __init(self, X_train, y_train):
+    def __init__(self, X_train, y_train):
         self.X_train = X_train
         self.y_train = y_train
+        try:
+            self.m, self.n = self.X_train.shape
+        except ValueError:
+            self.m, self.n = self.X_train.shape[0], 1
     
-    
-    
-    
+
+    def cost(self, w, b):
+        J = 0
+        for i in range(self.m):
+            J += ((np.dot(w, self.X_train[i] + b) - self.y_train[i])**2)
+        
+        return J/2*self.m
+
+
+    def train(self, learning_rate=0.01, nb_iters=100, _lambda=1):
+        w = np.zeros(self.n)
+        b = 0
+
+        for k in range(nb_iters):
+            for i in range(self.m):
+                f_wb = np.dot(self.X_train[i], w)+b - self.y_train[i]
+                w = w - learning_rate*f_wb*self.X_train[i]
+                b = b - learning_rate*f_wb
+            
+            if not k%10:
+                print(f"[+] Cost Function : {self.cost(w,b)}")
+
+        return w, b
